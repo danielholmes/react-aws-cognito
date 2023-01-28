c668e84e9b42953771fe589c29a85655d8f1f83e
Daniel Holmes <daniel@danielholmes.org>
2023-01-28 11:02:21 +1100

    Add lint staged and husky

---
+++ lint-staged.config.mjs
@@ -0,0 +1,17 @@
+// Split up to parallelise
+export default {
+    "src/**/*.ts": [
+      // "eslint --cache --cache-location ./node_modules/.cache/eslint --fix",
+      "prettier --write"
+    ],
+    "src/**/*.tsx": [
+      // "eslint --cache --cache-location ./node_modules/.cache/eslint --fix",
+      "prettier --write"
+    ],
+    "src/**/*.{ts,tsx}": [
+      // TSC can apparently take either filepath inputs, or a project config.
+      // lint-staged is passing in filename inputs, but we want the project config.
+      // empty function removes the filepaths
+      () => "tsc --noEmit",
+    ],
+  };
